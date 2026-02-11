61a287836e1d4056141c194e00e174e2441a8248
Gzilla999 <g999.zilla999@gmail.com>
2026-02-11 09:06:47 +0800

    Create sw.js
    Signed-off-by: Gzilla999 <g999.zilla999@gmail.com>

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "offline-cache-v1";
+const OFFLINE_FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(OFFLINE_FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
