b673c096ac8c13d72f31d19621fa208594350fc8
Gzilla999 <g999.zilla999@gmail.com>
2026-02-12 14:18:54 +0800

    Update sw.js
    Signed-off-by: Gzilla999 <g999.zilla999@gmail.com>

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* ===== CONFIG ===== */
-const CACHE_VERSION = "ms-cache"; // ⬅️ CHANGE THIS ON EVERY UPDATE
+const CACHE_VERSION = "ms-cache-v1.1.1"; // ⬅️ CHANGE THIS ON EVERY UPDATE
 const OFFLINE_ASSETS = [
   "./",
   "./index.html",
