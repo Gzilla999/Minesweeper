670a84807f045d7ea76adea5f4d2da66dcae33fe
GitHub Action <action@github.com>
2026-03-30 06:28:39 +0000

    chore: sync version to v1.3.4

--- README.md
+++ README.md
@@ -39,4 +39,4 @@ MIT License - see LICENSE file for details
 
 ---
 
-Current version: v1.3.3
+Current version: v1.3.4
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* ===== CONFIG ===== */
-const CACHE_VERSION = "ms-cache-v1.3.3";
+const CACHE_VERSION = "ms-cache-v1.3.4";
 const OFFLINE_ASSETS = [
   "./",
   "./index.html",
