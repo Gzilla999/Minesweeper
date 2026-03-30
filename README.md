ee48211ec402775c19ad063aeb1aecdbc530aa79
GitHub Action <action@github.com>
2026-03-30 03:35:02 +0000

    chore: sync version to v1.2.12

--- README.md
+++ README.md
@@ -39,4 +39,4 @@ MIT License - see LICENSE file for details
 
 ---
 
-Current version: v1.2.11
+Current version: v1.2.12
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* ===== CONFIG ===== */
-const CACHE_VERSION = "ms-cache-v1.2.11";
+const CACHE_VERSION = "ms-cache-v1.2.12";
 const OFFLINE_ASSETS = [
   "./",
   "./index.html",
